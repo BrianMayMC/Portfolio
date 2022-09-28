68b42e4a765620c0ef32c1d880c732d8b70f8b8a
BrianMay <brianmaymc@gmail.com>
2022-09-28 20:45:03 +0200

    Removed unnecessary projects

--- LuckyblockAirdrops/src/main/java/me/nootnoot/luckyblockairdrops/menus/menusystem/exceptions/InvalidInventorySize.java
+++
@@ -1,7 +0,0 @@
-package me.nootnoot.luckyblockairdrops.menus.menusystem.exceptions;
-
-public class InvalidInventorySize extends Exception{
-	public InvalidInventorySize(String s){
-		super(s);
-	}
-}
